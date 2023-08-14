d1357b2dc1875e933e487c3afb58fe48381fb656
netoudi <netoudi@rocketmail.com>
2023-08-14 10:21:00 -0300

    feat: add home page structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,88 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faArrowUpRightFromSquare, faBuilding, faUserGroup } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Box } from '@app/components/Box';
+import { Grid } from '@app/components/Grid';
+import { Heading } from '@app/components/Heading';
+import { Info } from '@app/components/Info';
+import { Post } from '@app/components/Post';
+import { Text } from '@app/components/Text';
 import * as S from './styles';
 
 export function Home() {
-  return <S.Wrapper>Home</S.Wrapper>;
+  return (
+    <S.Wrapper>
+      <S.Profile>
+        <Box>
+          <S.Content>
+            <S.Avatar>
+              <S.AvatarImage src="/images/avatar.png" alt="" />
+            </S.Avatar>
+            <S.Info>
+              <S.Header>
+                <Heading>Cameron Williamson</Heading>
+                <S.Link>
+                  <a href="https://github.com" target="_blank">
+                    Github
+                  </a>
+                  <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                </S.Link>
+              </S.Header>
+              <S.Bio>
+                <Text>
+                  Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam dignissim aenean
+                  malesuada suscipit. Nunc, volutpat pulvinar vel mass.
+                </Text>
+              </S.Bio>
+              <Info.Root>
+                <Info.Item>
+                  <Info.Icon>
+                    <FontAwesomeIcon icon={faGithub} />
+                  </Info.Icon>
+                  <Info.Label>cameronwll</Info.Label>
+                </Info.Item>
+                <Info.Item>
+                  <Info.Icon>
+                    <FontAwesomeIcon icon={faBuilding} />
+                  </Info.Icon>
+                  <Info.Label>Rocketseat</Info.Label>
+                </Info.Item>
+                <Info.Item>
+                  <Info.Icon>
+                    <FontAwesomeIcon icon={faUserGroup} />
+                  </Info.Icon>
+                  <Info.Label>32 seguidores</Info.Label>
+                </Info.Item>
+              </Info.Root>
+            </S.Info>
+          </S.Content>
+        </Box>
+      </S.Profile>
+
+      <S.Search>
+        <S.Header>
+          <Heading size="small">Publicações</Heading>
+          <Text size="small">6 publicações</Text>
+        </S.Header>
+        <form>
+          <input type="text" placeholder="Buscar conteúdo" />
+        </form>
+      </S.Search>
+
+      <S.Posts>
+        <Grid>
+          {[1, 2, 3, 4, 5, 6].map((value) => {
+            return (
+              <Post
+                key={value}
+                title="JavaScript data types and data structures"
+                summary="Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn."
+                datetime="Há 1 dia"
+              />
+            );
+          })}
+        </Grid>
+      </S.Posts>
+    </S.Wrapper>
+  );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,95 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { Container } from '@app/components/Container';
 
 export const Wrapper = styled.div``;
+
+///
+/// PROFILE
+///
+export const Profile = styled(Container)``;
+
+export const Content = styled.div`
+  display: flex;
+  gap: 3.2rem;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Link = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    gap: 0.8rem;
+
+    color: ${theme.colors.blue};
+
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.xsmall};
+    font-style: normal;
+    font-weight: ${theme.font.bold};
+    line-height: 0;
+    text-transform: uppercase;
+
+    a {
+      color: ${theme.colors.blue};
+      text-decoration: none;
+    }
+  `};
+`;
+
+export const Bio = styled.div`
+  margin-top: 0.8rem;
+  margin-bottom: 2.4rem;
+`;
+
+export const Info = styled.div``;
+
+export const Avatar = styled.div``;
+
+export const AvatarImage = styled.img`
+  width: 14.8rem;
+  height: 14.8rem;
+  border-radius: 0.8rem;
+  overflow: hidden;
+  object-fit: cover;
+`;
+
+///
+/// SEARCH
+///
+export const Search = styled(Container)`
+  ${({ theme }) => css`
+    padding-top: 7.2rem;
+    padding-bottom: 1.6rem;
+
+    form {
+      margin-top: 1.2rem;
+    }
+
+    input {
+      width: 100%;
+      padding: 1.2rem 1.6rem;
+
+      border-radius: 0.6rem;
+      border: 1px solid ${theme.colors['base-border']};
+      background: ${theme.colors['base-input']};
+
+      color: ${theme.colors['base-label']};
+
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+      font-style: normal;
+      font-weight: ${theme.font.normal};
+      line-height: 160%;
+    }
+  `};
+`;
+
+///
+/// POSTS
+///
+export const Posts = styled(Container)``;
