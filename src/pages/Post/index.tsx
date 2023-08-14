8881d2dbaa23d20f750433a1f66cff46da518c4b
netoudi <netoudi@rocketmail.com>
2023-08-14 11:30:30 -0300

    feat: add post page structure

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,5 +1,68 @@
+import ReactMarkdown from 'react-markdown';
+import { Link } from 'react-router-dom';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faArrowUpRightFromSquare, faCalendarDay, faChevronLeft, faComment } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Heading } from '@app/components/Heading';
+import { Info } from '@app/components/Info';
 import * as S from './styles';
 
+const content = `
+Programming languages all have built-in data structures, but these often differ from one language to
+another. This article attempts to list the built-in data structures available in JavaScript and what
+properties they have. These can be used to build other data structures. Wherever possible, comparisons with
+other languages are drawn.
+
+[Dynamic typing](https://github.com)
+
+JavaScript is a loosely typed and dynamic language. Variables in JavaScript are not directly associated with
+any particular value type, and any variable can be assigned (and re-assigned) values of all types:
+`;
+
 export function Post() {
-  return <S.Wrapper>Post</S.Wrapper>;
+  return (
+    <S.Wrapper>
+      <S.PostInfo>
+        <S.Content>
+          <S.Header>
+            <S.Link>
+              <FontAwesomeIcon icon={faChevronLeft} />
+              <Link to="/">voltar</Link>
+            </S.Link>
+            <S.Link>
+              <a href="https://github.com" target="_blank">
+                ver no github
+              </a>
+              <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+            </S.Link>
+          </S.Header>
+          <Heading size="large">JavaScript data types and data structures</Heading>
+          <Info.Root>
+            <Info.Item>
+              <Info.Icon>
+                <FontAwesomeIcon icon={faGithub} />
+              </Info.Icon>
+              <Info.Label>cameronwll</Info.Label>
+            </Info.Item>
+            <Info.Item>
+              <Info.Icon>
+                <FontAwesomeIcon icon={faCalendarDay} />
+              </Info.Icon>
+              <Info.Label>Há 1 dia</Info.Label>
+            </Info.Item>
+            <Info.Item>
+              <Info.Icon>
+                <FontAwesomeIcon icon={faComment} />
+              </Info.Icon>
+              <Info.Label>5 comentários</Info.Label>
+            </Info.Item>
+          </Info.Root>
+        </S.Content>
+      </S.PostInfo>
+
+      <S.PostContent>
+        <ReactMarkdown children={content} />
+      </S.PostContent>
+    </S.Wrapper>
+  );
 }
--- src/pages/Post/styles.ts
+++ src/pages/Post/styles.ts
@@ -1,3 +1,60 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { Box } from '@app/components/Box';
+import { Container } from '@app/components/Container';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled(Container)``;
+
+export const PostInfo = styled.div``;
+
+export const Content = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 1.2rem;
+`;
+
+export const Link = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    gap: 0.8rem;
+
+    color: ${theme.colors.blue};
+
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.xsmall};
+    font-style: normal;
+    font-weight: ${theme.font.bold};
+    line-height: 0;
+    text-transform: uppercase;
+
+    a {
+      color: ${theme.colors.blue};
+      text-decoration: none;
+    }
+  `};
+`;
+
+export const PostContent = styled.div`
+  ${({ theme }) => css`
+    padding: 4rem 3.2rem;
+    color: ${theme.colors['base-text']};
+
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.medium};
+    font-style: normal;
+    font-weight: ${theme.font.normal};
+    line-height: 160%;
+
+    a {
+      color: ${theme.colors.blue};
+      text-decoration: underline;
+    }
+  `};
+`;
